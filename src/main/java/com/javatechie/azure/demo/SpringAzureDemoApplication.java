3f4afbf07cc8c423c31e3386ec2cc9145df490f3
basahota <techbasanta757@gmail.com>
2021-05-12 17:08:49 +0530

    second commite

--- src/main/java/com/javatechie/azure/demo/SpringAzureDemoApplication.java
+++ src/main/java/com/javatechie/azure/demo/SpringAzureDemoApplication.java
@@ -11,7 +11,7 @@ public class SpringAzureDemoApplication {
 
 	@GetMapping("/message")
 	public String message(){
-		return "Congrats ! you app deployed successfully in Azure !";
+		return "Congrats ! your application deployed successfully in Azure Platform !";
 	}
 
 	public static void main(String[] args) {
